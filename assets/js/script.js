6d8f3d366971cf395f19d9bc7e911ba704a61a32
Joel C <joel7921111@yahoo.com>
2022-02-26 15:35:46 -0700

    html hardcode timeblocks

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//moment.js for current date new Date()
+//time blocks hardcoded w appropriate classes and ids to work w JS. color code them as past present future (moment.js checks for time)
+//use 24hr time 
+//create  save event button w listener and handler function
+//clicking time block allows text editing
+//save button stashes event data in localstorage so it persists
+//function for dynamically changing the textcontent of the timeblocks
+//loop through each time block and check each timeblock, comparing each hour (is present less than 13 or greater than 13)
+//parseInt(moment().format("HH"));
+//auditevent function to check time stuff see photo
